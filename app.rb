596c6466b7bad937f2963401724136b0822f10ae
Jasper Rosenberg <BioGaming@Jaspers-MacBook-Air.local>
2016-06-07 10:32:38 -0400

    fix

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get '/' do
 	erb :index
 end
 
-POST '/sign_up_process'
+POST '/sign_up_process' do
 	@post = User.create(username: params[:email], password: params[:password])
 	@post = Profile.create(fname: params[:fname], lname: params[:lname], user_id: User.find_by_username(params[:email]).id)
 	redirect
